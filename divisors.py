2f7a0153047ad49a997e9e46ef4c39636534fc82
Miklos Bowling <miklos.bowling@gmail.com>
2021-04-15 17:05:00 -0700

    Encapsulated divisor calc in method

--- divisors.py
+++ divisors.py
@@ -1,5 +1,9 @@
+def calc_divisors(x):
+    return [i for i in range(1, x + 1) if x % i == 0]
+
 print("Enter a number: ", end='')
 x = int(input())
 
+divisors = calc_divisors(x)
 print("Here are the divisors: ")
-print([i for i in range(1, x + 1) if x % i == 0])
+print(divisors)
