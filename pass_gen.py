700cf3cdca4c2ceb7582092cea2e0e8730d76f65
Miklos Bowling <miklos.bowling@gmail.com>
2021-04-15 17:03:35 -0700

    Print out password

--- pass_gen.py
+++ pass_gen.py
@@ -5,3 +5,5 @@ print("Input the length of password you'd like: ", end='')
 length = int(input())
 chars = string.ascii_lowercase
 password = ''.join(random.choice(chars) for i in range(length))
+print("Here's your password - don't forget it!")
+print(password)
