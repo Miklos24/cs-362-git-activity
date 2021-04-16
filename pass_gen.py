28dd0a56bd2641c0a3f363802add3a7c429637ef
Miklos Bowling <miklos.bowling@gmail.com>
2021-04-15 17:03:14 -0700

    Write password generation

--- pass_gen.py
+++ pass_gen.py
@@ -1,2 +1,7 @@
+import random
+import string
+
 print("Input the length of password you'd like: ", end='')
 length = int(input())
+chars = string.ascii_lowercase
+password = ''.join(random.choice(chars) for i in range(length))
